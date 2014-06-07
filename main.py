3a1773fc42629386ead3a6f00fe68527a4b21f6d
MariaBarrett <Maria@Macintosh-6.local>
2014-06-07 09:57:30 +0200

    train and test split

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ targets = pickle.load( open( "targets.p", "rb" ) )
 dataset = pickle.load( open( "dataset.p", "rb" ) )
 
 #Dataset structure: [[[sentence 1 in review1], [sentence 2 in review 1] ...], [[sentence 1 in review 2], [sentence 2 in review 2] ...] ...]
-# Dataset is shuffled and balanced
+# Dataset is shuffled and balanced, 1000 each of pos, neu and neg
+
+X_train = dataset[:2500]
+y_train = target[:2500]
+
+X_test = dataset[2500:]
+y_test = targets[2500:]
