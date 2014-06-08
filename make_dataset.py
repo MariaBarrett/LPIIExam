2b629f057d6147b08afe34c00c7fe505873452c7
MariaBarrett <Maria@Macintosh-6.local>
2014-06-08 11:07:16 +0200

    lowercased dataset

--- make_dataset.py
+++ make_dataset.py
@@ -26,7 +26,7 @@ def make_dataset():
 	    for row in reader:
 			targets.append(row[3])
 			temp = []
-			sentences = nltk.sent_tokenize(row[4])
+			sentences = nltk.sent_tokenize(row[4].lower())
 			for sent in sentences:
 				temp.append(nltk.word_tokenize(sent))
 			dataset.append(temp)
@@ -40,31 +40,32 @@ I shuffle and return a list og targets and a list of reviews.
 """
 def balancedataset(oversampled_targets, oversampled_dataset):
 	n = 1000
-	
-	# changing targets to 0 for negative, 1 for middle and 2 for positive
+
 	# here we can also chose to take out the rating 2 or 4 to get more defined classes
-	new_oversampled_t = [x if x != '1' else 0 for x in oversampled_targets]
-	new_oversampled_t = [x if x != '2' else 0 for x in new_oversampled_t]
-	new_oversampled_t = [x if x != '3' else 1 for x in new_oversampled_t]
-	new_oversampled_t = [x if x != '4' else 2 for x in new_oversampled_t]
-	new_oversampled_t = [x if x != '5' else 2 for x in new_oversampled_t]
+	rate1 = []
+	rate2 = []
+	rate3 = []
+	rate4 = []
+	rate5 = []
+
+	for i in xrange(len(oversampled_targets)):
+		if oversampled_targets[i] == '1':
+			rate1.append(oversampled_dataset[i])
+		elif oversampled_targets[i] == '2':
+			rate2.append(oversampled_dataset[i])
+		elif oversampled_targets[i] == '3':
+			rate3.append(oversampled_dataset[i])
+		elif oversampled_targets[i] == '4':
+			rate4.append(oversampled_dataset[i])
+		elif oversampled_targets[i] == '5':
+			rate5.append(oversampled_dataset[i])
+	
+	balanced_dataset = rate1[:420]+rate2[:580]+rate3[:1000]+rate4[:500]+rate5[:500]
+	print len(balanced_dataset)
+	# changing targets to 0 for negative, 1 for middle and 2 for positive
+	balanced_targets = [0]*1000+[1]*1000+[2]*1000
 
 	print "Done changing all values to 0,1 or 2"
-	neg_list = []
-	neu_list = []
-	pos_list = []
-
-	for i in xrange(len(new_oversampled_t)):
-		if new_oversampled_t[i] == 0:
-			neg_list.append(oversampled_dataset[i])
-		elif new_oversampled_t[i] == 1:
-			neu_list.append(oversampled_dataset[i])
-		elif new_oversampled_t[i] == 2:
-			pos_list.append(oversampled_dataset[i])
-	
-
-	balanced_dataset = neg_list[:1000]+neu_list[:1000]+pos_list[:1000]
-	balanced_targets = [0]*1000+[1]*1000+[2]*1000
 
 	zipped = zip(balanced_targets, balanced_dataset) #I'm zipping while shuffline because otherwise label and review did not match despite the fact that I seeded. Did I misunderstand something about seeding?
 	
